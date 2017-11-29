db23f16dcece49937eb3de2eeb82d6d45e8e8dc1
ruelbontuyan <ruelbontuyan@yahoo.com>
2017-11-30 01:03:59 +0800

    updated

--- topics/app/app.py
+++ topics/app/app.py
@@ -1,88 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from forms import TopicsForm
+from flask_wtf import Form
+from wtforms import StringField, IntegerField, validators, ValidationError
+from wtforms.validators import DataRequired, Length
 from sqlalchemy import func
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:0143@localhost/database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/mydbs'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db = SQLAlchemy(app)
 
 bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = 'flaskimp'
-
-
-class Enrolls(db.Model):
-    __tablename__ = 'Enrolls'
-
-    userid = db.Column(db.Integer, primary_key=True)
-    courseid = db.Column(db.Integer, primary_key=True)
-    score = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    rank = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, userid, courseid, score, date, rank):
-        self.userid = userid
-        self.courseid = courseid
-        self.score = score
-        self.date = date
-        self.rank = rank
-
-
-class Users(db.Model):
-    __tablename__ = 'Users'
-
-    userid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), nullable=False)
-    usertype = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, userid, username, usertype, password):
-        self.userid = userid
-        self.username = username
-        self.usertype = usertype
-        self.password = password
-
-
-class Courses(db.Model):
-    __tablename__ = 'Courses'
-
-    courseid = db.Column(db.Integer, primary_key=True)
-    coursename = db.Column(db.String(50), nullable=False)
-    coursedesc = db.Column(db.String(100), nullable=False)
-
-    def __init__(self, courseid, coursename, coursedesc):
-        self.courseid = courseid
-        self.coursename = coursename
-        self.coursedesc = coursedesc
-
-
-class Exams(db.Model):
-    __tablename__ = 'Exams'
-
-    examid = db.Column(db.Integer, primary_key=True)
-    examtype = db.Column(db.String(30), nullable=False)
-    courseid = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.Integer, primary_key=True)
-
-    def __init__(self, examid, examtype, courseid, userid):
-        self.examid = examid
-        self.examtype = examtype
-        self.courseid = courseid
-        self.userid = userid
+app.config['SECRET_KEY'] = 'wit'
 
 
 class Topics(db.Model):
-    __tablename__ = 'Topics'
+    __tablename__ = "topics"
 
     topicid = db.Column(db.Integer, primary_key=True)
-    topicname = db.Column(db.String(30), nullable=False)
-    topicdisc = db.Column(db.String(100), nullable=False)
-    courseid = db.Column(db.Integer)
+    topicname = db.Column(db.String(50), nullable=False)
+    topicdisc = db.Column(db.String(500), nullable=False)
+    courseid = db.Column(db.String(30), nullable=False)
 
     def __init__(self, topicname, topicdisc, courseid):
         self.topicname = topicname
@@ -90,104 +31,70 @@ class Topics(db.Model):
         self.courseid = courseid
 
     def __repr__(self):
-        return '<topicid {}>'.format(self.topicid)
+        return '<topicname {}>'.format(self.topicname)
 
 
-class Questions(db.Model):
-    __tablename__ = 'Questions'
+class TopicsForm(Form):
+    topicname = StringField('Topic', validators=[DataRequired()])
+    topicdisc = StringField('Discussion', validators=[DataRequired()])
+    courseid = StringField('Course Id', validators=[DataRequired()])
 
-    questionid = db.Column(db.Integer, primary_key=True)
-    difficulty = db.Column(db.String(30), nullable=False)
-    answer = db.Column(db.String(100), nullable=False)
-    examid = db.Column(db.Integer, primary_key=True)
-
-    def __init__(self, questionid, difficulty, answer, examid):
-        self.questionid = questionid
-        self.difficulty = difficulty
-        self.answer = answer
-        self.examid = examid
+class SearchForm(Form):
+    topicname = StringField('Topic', validators=[DataRequired()])
+    topicdisc = StringField('Discussion', validators=[DataRequired()])
+    courseid = IntegerField('Course id', validators=[DataRequired()])
 
 
-class Exercises(db.Model):
-    __tablename__ = 'Exercises'
-
-    exerciseid = db.Column(db.Integer, primary_key=True)
-    topicid = db.Column(db.Integer, primary_key=True)
-    questionid = db.Column(db.Integer, primary_key=True)
-
-    def __init__(self, exerciseid, topicid, questionid):
-        self.exerciseid = exerciseid
-        self.topicid = topicid
-        self.questionid = questionid
-
-
-class Choices(db.Model):
-    __tablename__ = 'Choices'
-
-    choicesid = db.Column(db.Integer, primary_key=True)
-    questionid = db.Column(db.Integer, primary_key=True)
-    choices = db.Column(db.String(30), nullable=False)
-
-    def __init__(self, choicesid, questionid, choices):
-        self.choicesid = choicesid
-        self.questionid = questionid
-        self.choices = choices
-
-
+db.create_all()
 app.debug = True
 
 
 @app.route('/', methods=['GET', 'POST'])
 def topics():
-    db.create_all()
-    result1 = Topics.query.order_by(Topics.topicname)
-    return render_template('topics.html', topics=result1, )
+    result1 = Topics.query.order_by(Topics.courseid)
+    return render_template('topics.html', topics=result1)
 
 
 @app.route('/add', methods=['POST', 'GET'])
 def addtopics():
-    form = TopicsForm(request.form)
+    form = TopicsForm()
     if request.method == 'POST':
-        if form.validate():
+        if form.validate_on_submit():
             topics = Topics(topicname=form.topicname.data, topicdisc=form.topicdisc.data, courseid=form.courseid.data)
             db.session.add(topics)
             db.session.commit()
-            return redirect(url_for('topics'))
-        else:
-            return render_template('addtopics.html', form=form)
-    else:
-        return render_template('addtopics.html', form=form)
+            result = Topics.query.order_by(Topics.courseid)
+            return redirect(url_for('topics', topics=result, form=form))
+    return render_template('addtopics.html', form=form)
 
 
-@app.route('/delete', methods=['GET', 'POST'])
-def deletetopics():
-    topics = Topics.query.order_by(Topics.topicname)
-    if request.method == 'POST':
-        Store = request.form['storage']
-        result = Topics.query.filter_by(topicname=Store).first()
-        db.session.delete(result)
-        db.session.commit()
-        topics = Topics.query.order_by(Topics.topicname)
-        return redirect(url_for('topics', topics=topics))
-    else:
-        return redirect(url_for('topics', topics=topics))
-
-
-@app.route('/update', methods=['POST', 'GET'])
-def updatetopics():
+@app.route('/edit/<topicid>', methods=['POST', 'GET'])
+def editrecords(topicid):
+    topics = Topics.query.filter_by(topicid=topicid).first()
     form = TopicsForm()
-    if request.method == 'POST':
-        if form.validate():
-            topics.topicname = form.topicname.data
-            topics.topicdisc = form.topicdisc.data
-            db.session.add(topics)
-            db.session.commit()
-            result = Topics.query.order_by(Topics.topicname)
-            return redirect(url_for('topics.html', topics=result))
-        else:
-            return render_template('updatetopics.html', form=form)
+    if form.validate_on_submit():
+        topics.topicname = form.topicname.data
+        topics.topicdisc = form.topicdisc.data
+        topics.courseid = form.courseid.data
+        db.session.add(topics)
+        db.session.commit()
+        result = Topics.query.order_by(Topics.courseid)
+        return redirect(url_for('topics', topics=result, form=form))
     else:
-        return render_template('topics.html', form=form)
+
+        form.topicname.data = topics.topicname
+        form.topicdisc.data = topics.topicdisc
+        form.courseid.data = topics.courseid
+    return render_template('addtopics.html', form=form)
+
+
+@app.route('/delete/<topicid>', methods=['GET', 'POST'])
+def remove(topicid):
+    result = Topics.query.filter_by(topicid=topicid).first()
+    db.session.delete(result)
+    db.session.commit()
+    topics = Topics.query.order_by(Topics.topicname)
+    return redirect(url_for('topics', topics=topics))
 
 
 if __name__ == '__main__':
--- topics/app/forms.py
+++
@@ -1,7 +0,0 @@
-from wtforms import Form, StringField, IntegerField, ValidationError, SubmitField, validators
-
-class TopicsForm(Form):
-    topicname = StringField('Topic Name', [validators.length(min=1, max=60)])
-    topicdisc = StringField('Topic Discussion', [validators.length(min=1, max=100)])
-    courseid = StringField('Course Id', [validators.length(min=1, max=60)])
-    submit = SubmitField("Submit")
