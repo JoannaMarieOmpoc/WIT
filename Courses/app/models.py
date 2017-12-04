068d2ff1fab34147b6dd009f7678a07570fff706
Cjbeleta <31245675+Cjbeleta@users.noreply.github.com>
2017-12-04 17:46:01 +0800

    updated model

--- Courses/app/models.py
+++ Courses/app/models.py
@@ -1,107 +1,57 @@
 from app import app,db
 
+class User(db.Model):
+	__tablename__ = 'Users'
+	username = db.Column(db.String(30), unique=True, primary_key=True, nullable=False)
+	usermail = db.Column(db.String(50), unique=True, nullable=False)
+	usertype = db.Column(db.String(10), nullable=False)
+	password = db.Column(db.String(50), nullable=False)
+	courses = db.relationship('Course', backref='User', secondary='Enrolled', lazy=True)
+
+	def __init__(self, username, usermail, usertype, password):
+		self.username = username
+		self.usermail = usermail
+		self.usertype = usertype
+		self.password = password
+
+	def is_authenticated(self):
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return self.username
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
 class Course(db.Model):
-    __tablename__ = "course"
-
-    courseId = db.Column(db.Integer, primary_key=True)
-    courseName = db.Column(db.String(50), nullable=False)
-    courseDesc = db.Column(db.String(500), nullable=False)
-
-    def __init__(self, courseId, courseName, courseDesc):
-        self.courseId = courseId
-        self.courseName = courseName
-        self.courseDesc = courseDesc
-
-    def __repr__(self):
-        return '<courseName {}>'.format(self.courseName)
-
-take = db.Table('taker',
-                db.Column('userid', db.Integer, db.ForeignKey('Users.userid')),
-                db.Column('examid', db.Integer, db.ForeignKey('Exams.examid'))
-                )
-stud = db.Table('stud',
-                    db.Column('userid', db.Integer, db.ForeignKey('Users.userid')),
-                    db.Column('courseid', db.Integer, db.ForeignKey('Courses.courseid'))
-                    )
-
-class Exams(db.Model):
-    __tablename__ = 'Exams'
-
-    examid = db.Column(db.Integer, primary_key=True)
-    examtype = db.Column(db.String(30), nullable=False)
-    courseid = db.Column(db.Integer, nullable=False)
-    takers = db.relationship('Exams', secondary=take, backref=db.backref('take', lazy='dynamic'))
-
-    def __init__(self, examid, examtype, courseid):
-        self.examid = examid
-        self.examtype = examtype
-        self.courseid = courseid
-
-
-class Questions(db.Model):
-    __tablename__ = 'Questions'
-
-    questionid = db.Column(db.Integer, primary_key=True)
-    difficulty = db.Column(db.String(30), nullable=False)
-    question = db.Column(db.String(100), nullable=False)
-    answer = db.Column(db.String(100), nullable=False)
-    examid = db.Column(db.Integer, primary_key=True)
-
-    def __init__(self, questionid, question, difficulty, answer, examid):
-        self.questionid = questionid
-        self.question = question
-        self.difficulty = difficulty
-        self.answer = answer
-        self.examid = examid
-
-class Users(db.Model):
-    __tablename__ = 'Users'
-
-    userid = db.Column(db.Integer, primary_key=True )
-    username = db.Column(db.String(30), nullable=False)
-    usertype = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.String(30), nullable=False)
-    courses = db.relationship('Courses', secondary=stud, backref=db.backref('students', lazy='dynamic'))
-
-    def __init__(self, userid, username, usertype, password):
-        self.userid = userid
-        self.username = username
-        self.usertype = usertype
-        self.password = password
-
-class Courses(db.Model):
     __tablename__ = 'Courses'
 
     courseid = db.Column(db.Integer, primary_key=True)
     coursename = db.Column(db.String(50), nullable=False)
-    coursedesc = db.Column(db.String(100), nullable=False)
+    coursedesc = db.Column(db.Text, nullable=False)
+    users = db.relationship('User', backref='Course', secondary='Enrolled', lazy=True)
 
-    def __init__(self, courseid, coursename, coursedesc):
-        self.courseid = courseid
+    def __init__(self, coursename, coursedesc):
         self.coursename = coursename
         self.coursedesc = coursedesc
 
-class Discussion(db.Model):
-    __tablename__ = 'Discussion'
+    def __repr__(self):
+        return '<courseName {}>'.format(self.coursename)
 
-    discussionid = db.Column(db.Integer, primary_key=True)
-    discussionname = db.Column(db.String(50), nullable=False)
-    discontent = db.Column(db.String(1000), nullable=False)
-    topicid = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, discussionid, discussionname, discontent, topicid):
-        self.discussionid = discussionid
-        self.discussionname = discussionname
-        self.discontent = discontent
-        self.topicid = topicid
-
-class Topics(db.Model):
+class Topic(db.Model):
     __tablename__ = 'Topics'
 
     topicid = db.Column(db.Integer, primary_key=True)
     topicname = db.Column(db.String(30), nullable=False)
-    topicdisc = db.Column(db.String(1000), nullable=False)
-    courseid = db.Column(db.Integer, nullable=False)
+    topicdisc = db.Column(db.Text, nullable=False)
+    courseid = db.Column(db.Integer, db.ForeignKey(Courses.courseid), nullable=False)
 
     def __init__(self, topicname, topicdisc, courseid):
         self.topicname = topicname
@@ -110,6 +60,60 @@ class Topics(db.Model):
 
     def __repr__(self):
         return '<topicid {}>'.format(self.topicid)
+
+
+class Question(db.Model):
+    __tablename__ = 'Questions'
+
+    questionid = db.Column(db.Integer, primary_key=True)
+    difficulty = db.Column(db.String(30), nullable=False)
+    question = db.Column(db.String(100), nullable=False)
+    answer = db.Column(db.String(100), nullable=False)
+    examid = db.Column(db.Integer, db.ForeignKey(Exams.examid), nullable=False)
+
+    def __init__(self, question, difficulty, answer, examid):
+        self.question = question
+        self.difficulty = difficulty
+        self.answer = answer
+        self.examid = examid
+
+class Choice(db.Model):
+	__tablename__ = 'Choices'
+
+	choiceid = db.Column(db.Integer, primary_key=True)
+	choice = db.Column(db.String(100), nullable=False)
+	question = db.Column(db.Integer, db.ForeignKey('Questions.questionid'))
+
+	def __init__(self, choice, question):
+		self.choice = choice
+		self.question = question
+
+class Exam(db.Model):
+    __tablename__ = 'Exams'
+
+    examid = db.Column(db.Integer, primary_key=True)
+    examtype = db.Column(db.String(30), nullable=False)
+    courseid = db.Column(db.Integer, db.ForeignKey(Courses.courseid), nullable=False)
+
+    def __init__(self, examtype, courseid):
+        self.examtype = examtype
+        self.courseid = courseid
+
+class Exercise(db.Model):
+	__tablename__ = 'Exercises'
+
+	exerciseid = db.Column(db.Integer, primary_key=True)
+	questions = db.relationship('Question', backref='Exercises', lazy=True)
+
+class Enroll(db.Model):
+	__tablename__ = 'Enrolled'
+
+	user = db.Column(db.ForeignKey('Users.username'), primary_key=True)
+	course = db.Column(db.ForeignKey('Courses.courseid'), primary_key=True)
+	#enroll_date = db.Column('date', db.DateTime, default=datetime.utcnow)
+
+	user = db.relationship('User', backref='Enrolled')
+	course = db.relationship('Course', backref='Enrolled')
 
 db.create_all()
 app.debug = True
